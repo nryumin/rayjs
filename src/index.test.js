57a868aa59ccc50d4ff46571317518846edb7443
nikita <nikita.ryumin@assaia.com>
2022-01-13 13:37:24 +0300

    Build scripts

--- src/index.test.js
+++ src/index.test.js
@@ -1,28 +1,30 @@
-import {jest} from '@jest/globals';
+import { jest } from '@jest/globals';
 import {
   containsPoint,
   formatInterval,
   contains,
   intersection,
-  intersects, mask,
+  intersects,
+  mask,
   NumInterval,
-  parseInterval, union
-} from "./index";
+  parseInterval,
+  union,
+} from './index';
 
 //parse test
 {
   const cases = [
-    [null,null],
-    [1,null],
-    [null,1],
-    [1,2],
-    [null,0],
-    [0,null],
-    [0,2],
-    [-3,0]
-  ]
+    [null, null],
+    [1, null],
+    [null, 1],
+    [1, 2],
+    [null, 0],
+    [0, null],
+    [0, 2],
+    [-3, 0],
+  ];
 
-  cases.forEach((i)=>{
+  cases.forEach((i) => {
     test(`Parse/format [${i[0]},${i[1]}]`, () => {
       const parsed = parseInterval(i);
       const returned = formatInterval(parsed);
@@ -34,27 +36,27 @@ import {
 //intersect test
 {
   const cases = [
-    [[null,2],[3,null],false],
-    [[null,3],[2,null],true],
-    [[3,6],[null,2],false],
-    [[3,6],[7,null],false],
-    [[3,6],[null,4],true],
-    [[3,6],[null,7],true],
-    [[3,6],[4,null],true],
-    [[3,6],[2,null],true],
-    [[3,6],[4,5],true],
-    [[3,6],[2,4],true],
-    [[3,6],[2,3],true],
-    [[3,6],[4,7],true],
-    [[3,6],[6,7],true],
-    [[3,6],[1,2],false],
-    [[3,6],[7,8],false],
-  ]
+    [[null, 2], [3, null], false],
+    [[null, 3], [2, null], true],
+    [[3, 6], [null, 2], false],
+    [[3, 6], [7, null], false],
+    [[3, 6], [null, 4], true],
+    [[3, 6], [null, 7], true],
+    [[3, 6], [4, null], true],
+    [[3, 6], [2, null], true],
+    [[3, 6], [4, 5], true],
+    [[3, 6], [2, 4], true],
+    [[3, 6], [2, 3], true],
+    [[3, 6], [4, 7], true],
+    [[3, 6], [6, 7], true],
+    [[3, 6], [1, 2], false],
+    [[3, 6], [7, 8], false],
+  ];
 
-  cases.forEach(([i1,i2,res])=>{
+  cases.forEach(([i1, i2, res]) => {
     test(`Intersection ${JSON.stringify(i1)} and ${JSON.stringify(i2)}`, () => {
-      expect(intersects(i1,i2)).toBe(res);
-      expect(intersects(i2,i1)).toBe(res);
+      expect(intersects(i1, i2)).toBe(res);
+      expect(intersects(i2, i1)).toBe(res);
     });
   });
 }
@@ -62,28 +64,28 @@ import {
 //contains test
 {
   const cases = [
-    [[null,2],[3,null],false],
-    [[null,3],[2,null],false],
-    [[null,3],[null,2],true],
-    [[null,2],[null,3],false],
-    [[2,null],[3,null],true],
-    [[3,null],[2,null],false],
-    [[3,null],[1,2],false],
-    [[3,null],[1,4],false],
-    [[3,null],[4,5],true],
-    [[null,6],[1,2],true],
-    [[null,6],[5,7],false],
-    [[null,6],[7,9],false],
-    [[4,8],[5,6],true],
-    [[4,8],[5,9],false],
-    [[4,8],[9,10],false],
-    [[4,8],[1,5],false],
-    [[4,8],[1,3],false],
-  ]
+    [[null, 2], [3, null], false],
+    [[null, 3], [2, null], false],
+    [[null, 3], [null, 2], true],
+    [[null, 2], [null, 3], false],
+    [[2, null], [3, null], true],
+    [[3, null], [2, null], false],
+    [[3, null], [1, 2], false],
+    [[3, null], [1, 4], false],
+    [[3, null], [4, 5], true],
+    [[null, 6], [1, 2], true],
+    [[null, 6], [5, 7], false],
+    [[null, 6], [7, 9], false],
+    [[4, 8], [5, 6], true],
+    [[4, 8], [5, 9], false],
+    [[4, 8], [9, 10], false],
+    [[4, 8], [1, 5], false],
+    [[4, 8], [1, 3], false],
+  ];
 
-  cases.forEach(([i1,i2,res])=>{
+  cases.forEach(([i1, i2, res]) => {
     test(`Contains ${JSON.stringify(i1)} and ${JSON.stringify(i2)}`, () => {
-      expect(contains(i1,i2)).toBe(res);
+      expect(contains(i1, i2)).toBe(res);
     });
   });
 }
@@ -91,22 +93,22 @@ import {
 //contains point
 {
   const cases = [
-    [[4,null],1,false],
-    [[4,null],4,true],
-    [[4,null],5,true],
-    [[null,4],1,true],
-    [[null,4],4,true],
-    [[null,4],5,false],
-    [[1,4],0,false],
-    [[1,4],1,true],
-    [[1,4],3,true],
-    [[1,4],4,true],
-    [[1,4],5,false],
-  ]
+    [[4, null], 1, false],
+    [[4, null], 4, true],
+    [[4, null], 5, true],
+    [[null, 4], 1, true],
+    [[null, 4], 4, true],
+    [[null, 4], 5, false],
+    [[1, 4], 0, false],
+    [[1, 4], 1, true],
+    [[1, 4], 3, true],
+    [[1, 4], 4, true],
+    [[1, 4], 5, false],
+  ];
 
-  cases.forEach(([i,p,res])=>{
+  cases.forEach(([i, p, res]) => {
     test(`Contains ${JSON.stringify(i)} point ${p}`, () => {
-      expect(containsPoint(i,p)).toBe(res);
+      expect(containsPoint(i, p)).toBe(res);
     });
   });
 }
@@ -114,28 +116,76 @@ import {
 //union test
 {
   const cases = [
-    [[null,2],[3,null],null],
-    [[null,3],[2,null],[null,null]],
-    [[null,3],[3,null],[null,null]],
-    [[null,3],[4,5],null],
-    [[null,3],[3,5],[null,5]],
-    [[null,3],[2,4],[null,4]],
-    [[null,4],[2,3],[null,4]],
-    [[5,null],[3,4],null],
-    [[5,null],[3,5],[3,null]],
-    [[5,null],[3,6],[3,null]],
-    [[5,null],[6,7],[5,null]],
-    [[1,5],[6,7],null],
-    [[1,5],[5,7],[1,7]],
-    [[1,5],[4,7],[1,7]],
-    [[1,5],[3,4],[1,5]],
-    [[1,5],[0,4],[0,5]],
-  ]
+    [[null, 2], [3, null], null],
+    [
+      [null, 3],
+      [2, null],
+      [null, null],
+    ],
+    [
+      [null, 3],
+      [3, null],
+      [null, null],
+    ],
+    [[null, 3], [4, 5], null],
+    [
+      [null, 3],
+      [3, 5],
+      [null, 5],
+    ],
+    [
+      [null, 3],
+      [2, 4],
+      [null, 4],
+    ],
+    [
+      [null, 4],
+      [2, 3],
+      [null, 4],
+    ],
+    [[5, null], [3, 4], null],
+    [
+      [5, null],
+      [3, 5],
+      [3, null],
+    ],
+    [
+      [5, null],
+      [3, 6],
+      [3, null],
+    ],
+    [
+      [5, null],
+      [6, 7],
+      [5, null],
+    ],
+    [[1, 5], [6, 7], null],
+    [
+      [1, 5],
+      [5, 7],
+      [1, 7],
+    ],
+    [
+      [1, 5],
+      [4, 7],
+      [1, 7],
+    ],
+    [
+      [1, 5],
+      [3, 4],
+      [1, 5],
+    ],
+    [
+      [1, 5],
+      [0, 4],
+      [0, 5],
+    ],
+  ];
 
-  cases.forEach(([i1,i2,res])=>{
+  cases.forEach(([i1, i2, res]) => {
     test(`Union ${JSON.stringify(i1)} and ${JSON.stringify(i2)}`, () => {
-      expect(union(i1,i2)).toEqual(res);
-      expect(union(i2,i1)).toEqual(res);
+      expect(union(i1, i2)).toEqual(res);
+      expect(union(i2, i1)).toEqual(res);
     });
   });
 }
@@ -143,59 +193,115 @@ import {
 //intersection test
 {
   const cases = [
-    [[null,2],[3,null],null],
-    [[null,3],[2,null],[2,3]],
-    [[null,3],[3,null],[3,3]],
-    [[null,3],[4,5],null],
-    [[null,3],[3,5],[3,3]],
-    [[null,3],[2,4],[2,3]],
-    [[null,4],[2,3],[2,3]],
-    [[5,null],[3,4],null],
-    [[5,null],[3,5],[5,5]],
-    [[5,null],[3,6],[5,6]],
-    [[5,null],[6,7],[6,7]],
-    [[1,5],[6,7],null],
-    [[1,5],[5,7],[5,5]],
-    [[1,5],[4,7],[4,5]],
-    [[1,5],[3,4],[3,4]],
-    [[1,5],[0,4],[1,4]],
-  ]
+    [[null, 2], [3, null], null],
+    [
+      [null, 3],
+      [2, null],
+      [2, 3],
+    ],
+    [
+      [null, 3],
+      [3, null],
+      [3, 3],
+    ],
+    [[null, 3], [4, 5], null],
+    [
+      [null, 3],
+      [3, 5],
+      [3, 3],
+    ],
+    [
+      [null, 3],
+      [2, 4],
+      [2, 3],
+    ],
+    [
+      [null, 4],
+      [2, 3],
+      [2, 3],
+    ],
+    [[5, null], [3, 4], null],
+    [
+      [5, null],
+      [3, 5],
+      [5, 5],
+    ],
+    [
+      [5, null],
+      [3, 6],
+      [5, 6],
+    ],
+    [
+      [5, null],
+      [6, 7],
+      [6, 7],
+    ],
+    [[1, 5], [6, 7], null],
+    [
+      [1, 5],
+      [5, 7],
+      [5, 5],
+    ],
+    [
+      [1, 5],
+      [4, 7],
+      [4, 5],
+    ],
+    [
+      [1, 5],
+      [3, 4],
+      [3, 4],
+    ],
+    [
+      [1, 5],
+      [0, 4],
+      [1, 4],
+    ],
+  ];
 
-  cases.forEach(([i1,i2,res])=>{
+  cases.forEach(([i1, i2, res]) => {
     test(`Intersection ${JSON.stringify(i1)} and ${JSON.stringify(i2)}`, () => {
-      expect(intersection(i1,i2)).toEqual(res);
-      expect(intersection(i2,i1)).toEqual(res);
+      expect(intersection(i1, i2)).toEqual(res);
+      expect(intersection(i2, i1)).toEqual(res);
     });
   });
 }
 
 //mask test
 {
-  const i = [5,20];
+  const i = [5, 20];
   const masks = [
-    [1,3],
-    [4,6],
-    [7,10],
-    [18,21],
-    [25,28]
+    [1, 3],
+    [4, 6],
+    [7, 10],
+    [18, 21],
+    [25, 28],
   ];
 
   test(`Mask ${JSON.stringify(i)} with ${JSON.stringify(masks)}`, () => {
-    const expected = [[5,6],[7,10],[18,20]]
-    expect(mask(i,masks)).toEqual(expected);
+    const expected = [
+      [5, 6],
+      [7, 10],
+      [18, 20],
+    ];
+    expect(mask(i, masks)).toEqual(expected);
   });
 }
 
 {
-  const i = [5,20];
+  const i = [5, 20];
   const masks = [
-    [null,6],
-    [7,10],
-    [18,null],
+    [null, 6],
+    [7, 10],
+    [18, null],
   ];
 
   test(`Mask ${JSON.stringify(i)} with ${JSON.stringify(masks)}`, () => {
-    const expected = [[5,6],[7,10],[18,20]]
-    expect(mask(i,masks)).toEqual(expected);
+    const expected = [
+      [5, 6],
+      [7, 10],
+      [18, 20],
+    ];
+    expect(mask(i, masks)).toEqual(expected);
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export type NumInterval = [number | null,number | null]
+export type NumInterval = [number | null, number | null];
 
-export function parseInterval(i: NumInterval) : [number,number] {
+export function parseInterval(i: NumInterval): [number, number] {
   return [i[0] === null ? -Infinity : i[0], i[1] === null ? Infinity : i[1]];
 }
 
-export function formatInterval([start,end]: [number,number]) : NumInterval {
+export function formatInterval([start, end]: [number, number]): NumInterval {
   const resStart = isFinite(start) ? start : null;
   const resEnd = isFinite(end) ? end : null;
-  return [resStart,resEnd];
+  return [resStart, resEnd];
 }
 
 export function intersects(i1: NumInterval, i2: NumInterval): boolean {
@@ -22,46 +22,43 @@ export function contains(i1: NumInterval, i2: NumInterval): boolean {
   const [start1, end1] = parseInterval(i1);
   const [start2, end2] = parseInterval(i2);
 
-  return start1 <= start2 && end1>=end2;
+  return start1 <= start2 && end1 >= end2;
 }
 
 export function containsPoint(i1: NumInterval, val: number): boolean {
   const [start, end] = parseInterval(i1);
-  return start <= val && end>=val;
+  return start <= val && end >= val;
 }
 
 export function union(i1: NumInterval, i2: NumInterval): NumInterval | null {
-  if(!intersects(i1,i2))
-    return null;
+  if (!intersects(i1, i2)) return null;
 
-  const [start1,end1] = parseInterval(i1);
-  const [start2,end2] = parseInterval(i2);
+  const [start1, end1] = parseInterval(i1);
+  const [start2, end2] = parseInterval(i2);
 
-  const start = Math.min(start1,start2);
-  const end = Math.max(end1,end2);
-  const res = formatInterval([start,end]);
+  const start = Math.min(start1, start2);
+  const end = Math.max(end1, end2);
+  const res = formatInterval([start, end]);
   return res;
 }
 
 export function intersection(i1: NumInterval, i2: NumInterval): NumInterval | null {
-  if(!intersects(i1,i2))
-    return null;
+  if (!intersects(i1, i2)) return null;
 
-  const [start1,end1] = parseInterval(i1);
-  const [start2,end2] = parseInterval(i2);
+  const [start1, end1] = parseInterval(i1);
+  const [start2, end2] = parseInterval(i2);
 
-  const start = Math.max(start1,start2);
-  const end = Math.min(end1,end2);
-  const res = formatInterval([start,end]);
+  const start = Math.max(start1, start2);
+  const end = Math.min(end1, end2);
+  const res = formatInterval([start, end]);
   return res;
 }
 
-export function mask(i: NumInterval,masks: NumInterval[]): NumInterval[] {
+export function mask(i: NumInterval, masks: NumInterval[]): NumInterval[] {
   const res: NumInterval[] = [];
-  masks.forEach(m=>{
-    const tmp = intersection(i,m);
-    if(tmp)
-      res.push(tmp);
-  })
+  masks.forEach((m) => {
+    const tmp = intersection(i, m);
+    if (tmp) res.push(tmp);
+  });
   return res;
 }
